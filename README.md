d72b5c67a49300f1e5a19bdc13d1d277f9291d97
jbpgithub <holy.torturer@gmail.com>
2017-02-03 22:57:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# jbp-address-dao
---
+++ src/main/java/UserDao.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Silver on 3.2.2017..
+ */
+public interface UserDao {
+    public void fetchUser();
+
+    public void findUsers();
+
+    public void authenticateUser();
+}
